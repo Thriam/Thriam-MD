8b6344092776c2136262d64d54dff1d9bbb1320e
Thriam <96582906+Thriam@users.noreply.github.com>
2022-04-30 16:42:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,27 +5,28 @@ const qrcode = require("qrcode-terminal")
 const { delay, useSingleFileAuthState } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState('./session.data.json')
 
-function qr() {
+function thriam() {
   let session = makeWASocket({
+    browser: ['Thriam','Safari','1.0.0'],
     auth: state,
     printQRInTerminal: true,
   })
   session.ev.on("connection.update", async (s) => {
     const { connection, lastDisconnect } = s
-    if (connection == "open") {
-      await delay(1000 * 10)
-      process.exit(0)
-    }
     if (
       connection === "close" &&
       lastDisconnect &&
       lastDisconnect.error &&
       lastDisconnect.error.output.statusCode != 401
     ) {
-      qr()
+      thriam()
     }
+    else if (connection == "open"){
+            await delay(1000 * 10)
+            process.exit(0)
+        }
   })
   session.ev.on('creds.update', saveState)
-  session.ev.on("messages.upsert", () => { })
+  session.ev.on('messages.upsert', () => {})
 }
-qr()
+thriam()
