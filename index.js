ee59c66d8b3ab16639d232789c551ee029316e1e
Thriam <96582906+Thriam@users.noreply.github.com>
2022-05-03 10:59:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //Made by Thriam bakesvar B
 
 const makeWASocket = require("@adiwajshing/baileys").default
+const { MessageType, MessageOptions, Mimetype } = require("@adiwajshing/baileys")
 const qrcode = require("qrcode-terminal")
 const { delay, useSingleFileAuthState } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState('./session.data.json')
 
-function thriam() {
+async function thriam() {
   let session = makeWASocket({
     auth: state,
     printQRInTerminal: true,
@@ -13,10 +14,6 @@ function thriam() {
   })
   session.ev.on("connection.update", async (s) => {
     const { connection, lastDisconnect } = s
-    if (connection == "open") {
-      await delay(1000 * 10)
-      process.exit(0)
-    }
     if (
       connection === "close" &&
       lastDisconnect &&
@@ -25,8 +22,16 @@ function thriam() {
     ) {
       thriam()
     }
+    if (connection == "open") {
+      const id = '919345588267@s.whatsapp.net'
+      const sentMsg1  = await session.sendMessage(id, { text: '*Successfully connected to Thriam API*' })
+      const sentMsg2  = await session.sendMessage(id, { text: 'Now you can use the session.data.json file that has been generated' })
+    }
   })
   session.ev.on('creds.update', saveState)
   session.ev.on("messages.upsert", () => { })
 }
-thriam()
+(async function() {
+    const response = await thriam();
+    console.log(response);
+})();
